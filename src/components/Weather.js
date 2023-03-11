3fe2818a4905b7a8207b4338d9a97d6bff85dc30
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2023-03-11 16:04:59 +0000

    update weather api

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,7 @@ function Weather(props){
     const [weather_news, setWeather_news] = useState(null);
 
     const getWeatherNews = async () => {
-        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=29.53&longitude=75.03&hourly=temperature_2m&current_weather=true');
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=29.53&longitude=75.03&current_weather=true');
         var jsonData = await response.json();
         setWeather_news(jsonData)
         console.log(weather_news.elevation)
@@ -17,10 +17,10 @@ function Weather(props){
           <div className="card" style={{width: "18rem"}}>
           <img src="https://upload.wikimedia.org/wikipedia/commons/0/0a/Sirsa_road.jpg" className="sirsa-image" alt="sirsa city"/>
           <div className="card-body">
-          <h5 className="card-title">{props.city} Weather</h5>
-          <p className="card-text">Latitude: {weather_news !== null && weather_news.latitude}</p>
-          <p className="card-text">Longitude: {weather_news !== null && weather_news.longitude}</p>
-          <a className="btn btn-success" onClick={getWeatherNews} type="button">Go Weather Info</a>
+          <h4 className="card-title">{props.city} Weather</h4>
+          <p className="card-text">Current Temprature: {weather_news !== null && weather_news.current_weather.temperature}</p>
+          <p className="card-text">Today's Date: {weather_news !== null && weather_news.current_weather.time}</p>
+          <button className="btn btn-success" onClick={getWeatherNews} type="button">Go Weather Info</button>
           </div>
           </div>
         </div>
