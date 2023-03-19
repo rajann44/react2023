41c1f95fea9a6067e1881353b0b42437e188c400
Rajan Chaudhary <rajann44@gmail.com>
2023-03-19 21:29:56 +0100

    check

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import './App.css';
-import { Route, Routes } from 'react-router';
-import Navbar from './components/Navbar';
-import Grid from './components/Grid';
-import Footer from './components/Footer';
-import ImageSlider from './components/ImageCarousel/ImageSlider';
-import RandomQuote from './components/Quotes/RandomQuote';
-import VideoPlayer from './components/VideoPlayer/VideoPlayer';
-import Bmi from './components/BMI/Bmi';
-import Movie from './components/Movie/Movie';
+import "./App.css";
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Grid from "./components/Grid";
+import Footer from "./components/Footer";
+import ImageSlider from "./components/ImageCarousel/ImageSlider";
+import RandomQuote from "./components/Quotes/RandomQuote";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+import Bmi from "./components/BMI/Bmi";
+import Movie from "./components/Movie/Movie";
 
 function App() {
   return (
     <>
-    {/*<Welcome name='Storage is still TODO' ></Welcome>
+      {/*<Welcome name='Storage is still TODOOO' ></Welcome>
     <Form></Form>
     <Weather city="Sirsa"></Weather>*/}
-    <Navbar/>
-    <Routes>
-        <Route path="/" element={<div className='container d-flex justify-content-center'><h1>Homepage</h1></div>} />
-        <Route path="/form-news" element={<><Grid/></>} />
-        <Route path="/image" element={<ImageSlider/>} />
-        <Route path="/quote" element={<RandomQuote/>} />
-        <Route path="/video" element={<VideoPlayer/>} />
-        <Route path="/bmi" element={<Bmi/>} />
-        <Route path="/movie" element={<Movie/>} />
-    </Routes>
-    <Footer/>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="container d-flex justify-content-center">
+              <h1>Homepage</h1>
+            </div>
+          }
+        />
+        <Route
+          path="/form-news"
+          element={
+            <>
+              <Grid />
+            </>
+          }
+        />
+        <Route path="/image" element={<ImageSlider />} />
+        <Route path="/quote" element={<RandomQuote />} />
+        <Route path="/video" element={<VideoPlayer />} />
+        <Route path="/bmi" element={<Bmi />} />
+        <Route path="/movie" element={<Movie />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
